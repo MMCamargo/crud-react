594e6523480338c56b6f749c8f3e9c4e9807f89f
MMCamargo <matheusmarianodecamargo@gmail.com>
2022-10-24 20:38:03 -0300

    #

--- src/pages/home/components/task-list-body/TaskListBody.tsx
+++ src/pages/home/components/task-list-body/TaskListBody.tsx
@@ -8,42 +8,43 @@ import { Button, Checkbox } from '@mui/material'
 function TaskListBody(): any {
 	const data: Array<Task> = taskData.map((value) => value)
 	const [dataBase, setDataBase] = useState(data)
-	console.log(dataBase)
 
-	return dataBase.forEach((value) => {
-		;<TaskListRows>
-			<TaskListCells>
-				<Checkbox />
-			</TaskListCells>
-			<TaskListCells>
-				<p className='task-list-title'>{value.title}</p>
-			</TaskListCells>
-			<TaskListCells>
-				<p className='task-list-description'>{value.description}</p>
-			</TaskListCells>
-			<TaskListCells>
-				<div className='task-list-actions'>
-					<Button
-						variant='outlined'
-						fullWidth={true}
-						onClick={() => {
-							alert('Você clicou no botão de edição!')
-						}}
-					>
-						Editar
-					</Button>
-					<Button
-						variant='contained'
-						fullWidth={true}
-						onClick={() => {
-							alert('Você clicou no botão de exclusão!')
-						}}
-					>
-						Excluir
-					</Button>
-				</div>
-			</TaskListCells>
-		</TaskListRows>
+	dataBase.forEach((value) => {
+		return (
+			<TaskListRows>
+				<TaskListCells>
+					<Checkbox />
+				</TaskListCells>
+				<TaskListCells>
+					<p className='task-list-title'>{value.title}</p>
+				</TaskListCells>
+				<TaskListCells>
+					<p className='task-list-description'>{value.description}</p>
+				</TaskListCells>
+				<TaskListCells>
+					<div className='task-list-actions'>
+						<Button
+							variant='outlined'
+							fullWidth={true}
+							onClick={() => {
+								alert('Você clicou no botão de edição!')
+							}}
+						>
+							Editar
+						</Button>
+						<Button
+							variant='contained'
+							fullWidth={true}
+							onClick={() => {
+								alert('Você clicou no botão de exclusão!')
+							}}
+						>
+							Excluir
+						</Button>
+					</div>
+				</TaskListCells>
+			</TaskListRows>
+		)
 	})
 }
 
