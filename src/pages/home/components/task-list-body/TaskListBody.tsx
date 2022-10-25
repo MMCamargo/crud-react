149a5f29aa58c3c253bb1c68be4a44e7f9a7b907
MMCamargo <matheusmarianodecamargo@gmail.com>
2022-10-25 19:53:31 -0300

    Atualização dinâmica das tasks

--- src/pages/home/components/task-list-body/TaskListBody.tsx
+++ src/pages/home/components/task-list-body/TaskListBody.tsx
@@ -1,51 +1,44 @@
 import taskData from '../../../../config/data/taskData'
-import Task from '../../../../config/data/interfaces/task/task'
-import { useState } from 'react'
 import TaskListRows from '../task-list-rows/TaskListRows'
 import TaskListCells from '../task-list-cells/TaskListCells'
 import { Button, Checkbox } from '@mui/material'
 
 function TaskListBody(): any {
-	const data: Array<Task> = taskData.map((value) => value)
-	const [dataBase, setDataBase] = useState(data)
-
-	dataBase.forEach((value) => {
-		return (
-			<TaskListRows>
-				<TaskListCells>
-					<Checkbox />
-				</TaskListCells>
-				<TaskListCells>
-					<p className='task-list-title'>{value.title}</p>
-				</TaskListCells>
-				<TaskListCells>
-					<p className='task-list-description'>{value.description}</p>
-				</TaskListCells>
-				<TaskListCells>
-					<div className='task-list-actions'>
-						<Button
-							variant='outlined'
-							fullWidth={true}
-							onClick={() => {
-								alert('Você clicou no botão de edição!')
-							}}
-						>
-							Editar
-						</Button>
-						<Button
-							variant='contained'
-							fullWidth={true}
-							onClick={() => {
-								alert('Você clicou no botão de exclusão!')
-							}}
-						>
-							Excluir
-						</Button>
-					</div>
-				</TaskListCells>
-			</TaskListRows>
-		)
-	})
+	return taskData.map((value) => (
+		<TaskListRows>
+			<TaskListCells>
+				<Checkbox />
+			</TaskListCells>
+			<TaskListCells>
+				<p className='task-list-title'>{value.title}</p>
+			</TaskListCells>
+			<TaskListCells>
+				<p className='task-list-description'>{value.description}</p>
+			</TaskListCells>
+			<TaskListCells>
+				<div className='task-list-actions'>
+					<Button
+						variant='outlined'
+						fullWidth={true}
+						onClick={() => {
+							alert('Você clicou no botão de edição!')
+						}}
+					>
+						Editar
+					</Button>
+					<Button
+						variant='contained'
+						fullWidth={true}
+						onClick={() => {
+							alert('Você clicou no botão de exclusão!')
+						}}
+					>
+						Excluir
+					</Button>
+				</div>
+			</TaskListCells>
+		</TaskListRows>
+	))
 }
 
 export default TaskListBody
